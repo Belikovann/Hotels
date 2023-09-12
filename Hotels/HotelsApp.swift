c11d79913f78d7e0a9ca43f72ff667e8f7793b72
@Annabel_ <hydrant94@gmail.com>
2023-09-12 20:49:25 +0200

    Initial Commit

---
+++ Hotels/HotelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelsApp.swift
+//  Hotels
+//
+//  Created by Аня Беликова on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
